c6885fda275a585990a6f56a66b7c49c567b1090
AdirIluz <adir.iluz76@gmail.com>
2023-06-24 22:38:35 +0300

    tictactoe

---
+++ script.js
@@ -0,0 +1,106 @@
+const tiles = document.querySelectorAll(".tile");
+const Player1 = 'X';
+const Player2 = 'O';
+let turn = Player1;
+
+const boardState = Array(tiles.length).fill(null);
+
+const strike = document.getElementById("strike");
+const gameOverArea = document.getElementById("game-over-area");
+const gameOverText = document.getElementById("game-over-text");
+const playAgain = document.getElementById("play-again");
+playAgain.addEventListener("click", startNewGame);
+
+const winningCombinations = [
+    // rows
+    { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+    { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+    { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+    // columns
+    { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+    { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+    { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+    // diagonals
+    { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+    { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+];
+
+tiles.forEach(tile => tile.addEventListener('click', tileClick));
+
+function setHoverText() {
+    tiles.forEach(tile => {
+        tile.classList.remove("x-hover");
+        tile.classList.remove("o-hover");
+    });
+
+    const hoverClass = `${turn.toLowerCase()}-hover`;
+
+    tiles.forEach(tile => {
+        if (tile.innerText == "") {
+            tile.classList.add(hoverClass);
+        }
+    });
+}
+
+function checkWinner() {
+    for (const winningCombination of winningCombinations) {
+        const { combo, strikeClass } = winningCombination;
+        const tileValue1 = boardState[combo[0] - 1];
+        const tileValue2 = boardState[combo[1] - 1];
+        const tileValue3 = boardState[combo[2] - 1];
+
+        if (tileValue1 != null && tileValue1 === tileValue2 && tileValue1 === tileValue3) {
+            strike.classList.add(strikeClass);
+            gameOverScreen(tileValue1);
+            return;
+        }
+        const allTileFilledIn = boardState.every((tile) => tile !== null);
+        if (allTileFilledIn) {
+            gameOverScreen(null)
+        }
+    }
+}
+
+function gameOverScreen(winnerText) {
+    let text = "Draw!"
+    if (winnerText != null) {
+        text = `The Winner is ${winnerText}!`;
+    }
+    gameOverArea.classList = "visible";
+    gameOverText.innerText = text;
+}
+
+function tileClick(event) {
+    if (gameOverArea.classList.contains('visible')) {
+        return;
+    }
+
+    const tile = event.target;
+    const tileNumber = tile.dataset.index;
+
+    if (tile.innerText != "") {
+        return;
+    }
+
+    if (turn === Player1) {
+        tile.innerText = Player1;
+        boardState[tileNumber - 1] = Player1;
+        turn = Player2;
+    } else {
+        tile.innerText = Player2;
+        boardState[tileNumber - 1] = Player2;
+        turn = Player1;
+    }
+
+    setHoverText();
+    checkWinner();
+}
+
+function startNewGame() {
+    strike.className = "strike";
+    gameOverArea.className = "hidden";
+    boardState.fill(null);
+    tiles.forEach((tile) => (tile.innerText = ""));
+    turn = Player1;
+    setHoverText();
+}
